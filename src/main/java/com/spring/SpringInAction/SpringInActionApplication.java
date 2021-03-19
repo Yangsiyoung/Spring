3a956926e764b730c2c233115c7918cb0c5607f3
Yangsiyoung <ysjleader@gmail.com>
2021-03-19 23:14:39 +0900

    스터디 여섯번째

--- src/main/java/com/spring/SpringInAction/SpringInActionApplication.java
+++ src/main/java/com/spring/SpringInAction/SpringInActionApplication.java
@@ -2,9 +2,6 @@ package com.spring.SpringInAction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 //@SpringBootConfiguration
@@ -16,11 +13,6 @@ public class SpringInActionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringInActionApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 
 }
